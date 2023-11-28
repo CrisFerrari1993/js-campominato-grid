f21abebea4427cc7f5bca372d0ba7e5181099fbb
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-28 18:07:09 +0100

    Consegna bonus :)

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -8,52 +8,173 @@ Consegna)
 // dichiarazione della variabile bottone
 const playButton = document.getElementById('play');
 console.log(playButton);
-
+// dichiarazione variabile dell'area di gioco
 const playArea = document.querySelector('.bg-acquamarine');
 console.log(playArea);
-
+//aggiungo nodo di testo dentro il bottone
+let play = document.createTextNode('Play');
+playButton.append(play);
 // event listener al bottone
 playButton.addEventListener('click', function(){
 
-    // dichiarazione della variabile grid
-    let grid = document.createElement('div');
-    // gli aggiungo la calsse grid
-    grid.classList.add('grid');
-    console.log(grid);
-    //al click si aggancia l'elemento creato
-    playArea.append(grid);
+    // dichiarazione variabile della difficoltà
+    let difficult = document.getElementById('difficolta');
+    //assegnazione valore in base alla scelta dell'utente
+    difficult = difficult.value;
+    
+    if (difficult === 'easy'){
+        //se si riclicca su play, la pagina verrà azzerata
+        playButton.removeChild(play);
+        let retry = document.createTextNode('Riprova');
+        playButton.append(retry);
+        playButton.addEventListener('click',
+            function(){
+                alert('Comincio nuova partita');
+                history.go(0);
+            }
+        );
+        // dichiarazione della variabile grid
+        let grid = document.createElement('div');
+        // gli aggiungo la calsse grid
+        grid.classList.add('gride');
+        console.log(grid);
+        //al click si aggancia l'elemento creato
+        playArea.append(grid);
 
-    //creazione quadratini nella griglia principale
-    //ciclo 100 volte la creazione del quadratino
-    for (let i = 1; i <= 100; i++){
-        //dichiaro la variabile n che farà da contatore aLLe interazioni
-        let n = i;
-        //dichiaro la funzione che crea i quadrati
-        const newElement = creaElem('div', 'square');
-        //creo la variabile che contiene il numero della casella iterata
-        let nCasella = document.createTextNode(`${n}`);
-        //e lo appendo alla casella generata
-        newElement.append(nCasella);
-        //evento click sulla casella iterata
-        newElement.addEventListener('click',
-        function() {
-            console.log('Hai cliccato la casella n°' + i);
-            newElement.classList.add('clicked');
+        //creazione quadratini nella griglia principale
+        //ciclo 100 volte la creazione del quadratino
+        for (let i = 1; i <= 100; i++) {
+            //dichiaro la variabile n che farà da contatore aLLe interazioni
+            let n = i;
+            //dichiaro la funzione che crea i quadrati
+            const newElement = creaElem('div', 'square');
+            //creo la variabile che contiene il numero della casella iterata
+            let nCasella = document.createTextNode(`${n}`);
+            //e lo appendo alla casella generata
+            newElement.append(nCasella);
+            //evento click sulla casella iterata
+            newElement.addEventListener('click',
+                function () {
+                    console.log('Hai cliccato la casella n°' + i);
+                    newElement.classList.add('clicked');
+                }
+
+            );
+            //appendo alla griglia 100 volte la funzione che crea i quadratini
+            grid.append(newElement);
+
         }
-        
-       );
-       //appendo alla griglia 100 volte la funzione che crea i quadratini
-       grid.append(newElement);
-       
-    }
 
-    //funzione crea quadratimi
-    function creaElem(tagType, classname) {
-        const elementoCorrente = document.createElement(tagType);
-        elementoCorrente.classList.add(classname);
-        
+        //funzione crea quadratimi
+        function creaElem(tagType, classname) {
+            const elementoCorrente = document.createElement(tagType);
+            elementoCorrente.classList.add(classname);
 
-        return elementoCorrente;
+
+            return elementoCorrente;
+        }
+    } else if (difficult === 'medium'){
+        //se si riclicca su play, la pagina verrà azzerata
+        playButton.removeChild(play);
+        let retry = document.createTextNode('Riprova');
+        playButton.append(retry);
+        playButton.addEventListener('click',
+            function () {
+                alert('Comincio nuova partita');
+                history.go(0);
+            }
+        );
+        // dichiarazione della variabile grid
+        let grid = document.createElement('div');
+        // gli aggiungo la calsse grid
+        grid.classList.add('gridm');
+        console.log(grid);
+        //al click si aggancia l'elemento creato
+        playArea.append(grid);
+
+        //creazione quadratini nella griglia principale
+        //ciclo 100 volte la creazione del quadratino
+        for (let i = 1; i <= 81; i++) {
+            //dichiaro la variabile n che farà da contatore aLLe interazioni
+            let n = i;
+            //dichiaro la funzione che crea i quadrati
+            const newElement = creaElem('div', 'squarem');
+            //creo la variabile che contiene il numero della casella iterata
+            let nCasella = document.createTextNode(`${n}`);
+            //e lo appendo alla casella generata
+            newElement.append(nCasella);
+            //evento click sulla casella iterata
+            newElement.addEventListener('click',
+                function () {
+                    console.log('Hai cliccato la casella n°' + i);
+                    newElement.classList.add('clicked');
+                }
+
+            );
+            //appendo alla griglia 100 volte la funzione che crea i quadratini
+            grid.append(newElement);
+
+        }
+
+        //funzione crea quadratimi
+        function creaElem(tagType, classname) {
+            const elementoCorrente = document.createElement(tagType);
+            elementoCorrente.classList.add(classname);
+
+
+            return elementoCorrente;
+        }
+    } else {
+        //se si riclicca su play, la pagina verrà azzerata
+        playButton.removeChild(play);
+        let retry = document.createTextNode('Riprova');
+        playButton.append(retry);
+        playButton.addEventListener('click',
+            function () {
+                alert('Comincio nuova partita');
+                history.go(0);
+            }
+        );
+        // dichiarazione della variabile grid
+        let grid = document.createElement('div');
+        // gli aggiungo la calsse grid
+        grid.classList.add('gridh');
+        console.log(grid);
+        //al click si aggancia l'elemento creato
+        playArea.append(grid);
+
+        //creazione quadratini nella griglia principale
+        //ciclo 100 volte la creazione del quadratino
+        for (let i = 1; i <= 64; i++) {
+            //dichiaro la variabile n che farà da contatore aLLe interazioni
+            let n = i;
+            //dichiaro la funzione che crea i quadrati
+            const newElement = creaElem('div', 'squareh');
+            //creo la variabile che contiene il numero della casella iterata
+            let nCasella = document.createTextNode(`${n}`);
+            //e lo appendo alla casella generata
+            newElement.append(nCasella);
+            //evento click sulla casella iterata
+            newElement.addEventListener('click',
+                function () {
+                    console.log('Hai cliccato la casella n°' + i);
+                    newElement.classList.add('clicked');
+                }
+
+            );
+            //appendo alla griglia 100 volte la funzione che crea i quadratini
+            grid.append(newElement);
+
+        }
+
+        //funzione crea quadratimi
+        function creaElem(tagType, classname) {
+            const elementoCorrente = document.createElement(tagType);
+            elementoCorrente.classList.add(classname);
+
+
+            return elementoCorrente;
+        }
     }
     }
 );
